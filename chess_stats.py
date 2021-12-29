8aef3144e76a08155cfa335c11f4290a59997529
jeremysteele <jeremysteelewriter@gmail.com>
2021-12-29 14:00:11 -0800

    2.0 changes

--- chess_stats.py
+++ chess_stats.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from datetime import time
 from pytz import timezone
 from pprint import pprint
-from pytz import timezone
 import pytz
 import pandas as pd
 
@@ -33,17 +32,30 @@ class PlayerStats:
     def query_archive_chess_site(self, month_url: object):
         """ Loop through the result to get a list of the games """
         monthly_games = requests.get(month_url).json()
+        monthly_class = Monthly_Dictionary()
+        sql_time = Sql_Info(self._name)
+        sql_time.check_to_create_table()
 
         for game in monthly_games['games']:
             game_instance = Chessgame(game)
-            Sql_Info(self._name, game_instance.chessgame_to_list())
+            dic = game_instance.chessgame_to_dict()
+            print(dic)
+
+            monthly_class.append_to_list(dic)
+
+        y = monthly_class.create_pandas_dataframe()
+        sql_time.insert_table_to_sql(y)
+
+    def get_archive_url(self):
+        """ Get Function """
+        return self._archives['archives'][10]
+
 
     def create_directories(self):
         """ Functions that creates folders if they dont already exist """
         path = (f"./graphs/{self._name}")
         time_path = (f"./graphs/{self._name}/time_based")
         opening_path = (f"./graphs/{self._name}/openings")
-
         # Making folders to save username specific data       
         try:
             os.makedirs(path)
@@ -51,7 +63,6 @@ class PlayerStats:
             print("Creation of the directory %s failed" % path)
         else:
             print("Successfully created the directory %s " % path)
-
         # Creating Opening graph folder within graphs
         try:
             os.makedirs(opening_path)
@@ -59,7 +70,6 @@ class PlayerStats:
             print("Creation of the directory %s failed" % opening_path)
         else:
             print("Successfully created the directory %s " % opening_path)
-
         # Creating Time graph folder within graphs
         try:
             os.makedirs(time_path)
@@ -68,15 +78,11 @@ class PlayerStats:
         else:
             print("Successfully created the directory %s" % time_path)
 
-    def get_archive_url(self):
-        """ Get Function """
-        return self._archives['archives'][3]
-
 class Chessgame:
     """ An instance of a single game with relevant information """
     
     def __init__(self, game_json):
-        """ Initializes the Chessgame object with all the relevant information by getting the raw JSON file from PlayerStats class"""
+        """ Initializes the Chessgame object by getting the API returned JSON information from chess.com """
         self._game_json = game_json
         self._fmt = "%Y.%m.%d %H:%M:%S"
         self._tz = timezone("US/Pacific")
@@ -89,7 +95,7 @@ class Chessgame:
         self._pgn = game_json['pgn']
         self._eco_code = self.format_eco()
         self._eco_name = self.format_eco_name()
-        self._date = self.format_date()
+        self._date = pd.to_datetime(self.format_date())
         self._time = self.format_time()
         self._white_bool = self.format_white_bool()
         self._black_bool = self.format_black_bool()
@@ -97,7 +103,6 @@ class Chessgame:
         self._time_control = game_json['time_control']
         self._rules = game_json['rules']
     
-
     def format_eco(self):
         """ Grabs the relevant eco_code from the pgn string """
         eco1 = re.sub("\W", "", self.get_chessgame_pgn().split('\n')[9].split(" ")[1])
@@ -150,35 +155,70 @@ class Chessgame:
         count = 1
         pgn_list = ""
         
-        for elem in self.get_chessgame_pgn().split("\n")[22].split(" "):
-            if count == 1:
-                pgn_list = pgn_list + elem + " "
-            elif count == 2:
-                pgn_list = pgn_list + elem + " "
-            elif count == 3:
-                pass
-            else: 
-                count = 0
-            count +=1
-        return pgn_list
+        try:
+            for elem in self.get_chessgame_pgn().split("\n")[22].split(" "):
+                if count == 1:
+                    pgn_list = pgn_list + elem + " "
+                elif count == 2:
+                    pgn_list = pgn_list + elem + " "
+                elif count == 3:
+                    pass
+                else: 
+                    count = 0
+                count +=1
+            return pgn_list
+        
+        except:
+            return None
 
     def get_chessgame_pgn(self):
         return self._pgn
 
-    def chessgame_to_list(self):
-        return self._date, 0, self._time_control, self._white_name, self._white_rating, self._white_bool, self._black_name, self._black_rating, self._black_bool, self._eco_code, self._eco_name, self._formatted_pgn
+    def chessgame_to_dict(self):
+        """ Creates a dictionary object of information to load into the Pandas Dataframe"""
+        
+        pandas_dict = {"date": self._date, "time": self._time, "time_control": self._time_control, "white_name": self._white_name, "white_rating": self._white_rating, "white_bool": self._white_bool, "black_name": self._black_name, "black_rating": self._black_rating, "black_bool": self._black_bool, "eco_code": self._eco_code, "eco_name": self._eco_name, "pgn_score": self._formatted_pgn}
+        
+        return pandas_dict
 
+class Monthly_Dictionary:
+    def __init__(self):
+        #self._dataframe = pd.DataFrame(data=None, columns=["date", "time", "time_control", "white_name", "white_rating", "white_bool", "black_name", "black_rating", "black_bool", "eco_code", "eco_name", "pgn_score"])
+        self._list_of_dict = []
+
+    def append_to_list(self, pandas_dictionary):
+        """ Receives the chessgame dictionary and appends it to our List, to create DataFrame at end. Apparently this method is faster than appending to the Dataframe """
+        self._list_of_dict.append(pandas_dictionary)
+
+    def create_pandas_dataframe(self):
+        """ Creates the Pandas DataFrame object at the end of the month """
+        df = pd.DataFrame(self._list_of_dict)
+        return df
 
 class Sql_Info:
-    def __init__(self, username, chessgame_object):
-        """ """
+    def __init__(self, username):
+        """ Initializes the SQL connection, and SQL table information """
         self._username = username
-        self._chessgame_object = chessgame_object
         self._db_url = 'postgresql://' + login + '@localhost:5432/chess_db'
         self._engine = create_engine(self._db_url)
+        
+    def insert_table_to_sql(self, pandas_table):
+        """ Function that receives a Pandas DataFrame and inserts it into SQL """
+        
         self._connection = self._engine.connect()
+        self._pandas_table = pandas_table
+        self._pandas_table = self._pandas_table.set_index('date')
+        
+        # query = f"INSERT INTO {self._username} (date, time, time_control, white_name, white_rating, white_bool, black_name, black_rating, black_bool, eco_code, eco_name, pgn_score) VALUES {self._pandas_table}"
+        
+        self._pandas_table.to_sql(self._username, self._engine, if_exists='append')
+        self._connection.close()
 
-        create_table = (f"CREATE TABLE IF NOT EXISTS {username} ( \
+    
+    def check_to_create_table(self):
+        """ Creates the table for the player """
+        self._connection = self._engine.connect()
+        create_table = (f"CREATE TABLE IF NOT EXISTS {self._username} ( \
 	    date date, \
 	    time varchar(40), \
 	    time_control varchar(10), \
@@ -193,9 +233,6 @@ class Sql_Info:
 	    pgn_score varchar(5000));")
 
         self._engine.execute(create_table)
-        
-        query = f"INSERT INTO {self._username} (date, time, time_control, white_name, white_rating, white_bool, black_name, black_rating, black_bool, eco_code, eco_name, pgn_score) VALUES {self._chessgame_object}"
-        self._connection.execute(query)
         self._connection.close()
 
 if __name__ == "__main__":
